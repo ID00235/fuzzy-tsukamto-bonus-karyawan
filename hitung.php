942891e54ae305089f61e61c10663e58c7ddb024
DESKTOP-AMHMHP3\asusx4y4 <irwanka.email@gmail.com>
2018-02-11 00:02:53 +0700

    rapikan hitungamndghjk

--- hitung.php
+++ hitung.php
@@ -12,7 +12,7 @@
       }else if($masakerja>2 && $masakerja<=5){
           $masakerjaLama = 0;
       }else if($masakerja>5 && $masakerja<=8){
-        $masakerjaLama=round(($masakerja - 5) /(8 - 5),2);
+        $masakerjaLama=($masakerja - 5) /(8 - 5);
       }else{
         $masakerjaLama = 1;
       }
@@ -20,10 +20,10 @@
       if($masakerja <=3 ){
         $masakerjaSedang = 0;
       }else if($masakerja>3 && $masakerja<=5){
-         $masakerjaSedang = round(($masakerja-3)/(5-3),2);
+         $masakerjaSedang = ($masakerja-3)/(5-3);
       }
       else if ($masakerja>5 && $masakerja<7){
-        $masakerjaSedang=round((7 - $masakerja) /(7 - 5),2);
+        $masakerjaSedang=(7 - $masakerja) /(7 - 5);
       }else{
          $masakerjaSedang =0;
       }
@@ -33,7 +33,7 @@
         $masakerjaBaru = 1;
 
       }else if($masakerja>2 && $masakerja<5){
-        $masakerjaBaru = round((5-$masakerja)/(5-2),2);
+        $masakerjaBaru = (5-$masakerja)/(5-2);
 
       }else{
         $masakerjaBaru = 0;
@@ -47,7 +47,7 @@
 
       }
       else if($gaji>3 && $gaji<=5){
-        $gajiBanyak=round(($gaji- 3) /(5 - 3),2);
+        $gajiBanyak= ($gaji- 3) /(5 - 3);
       }else{
         $gajiBanyak = 1;
       }
@@ -58,7 +58,7 @@
         $gajiSedikit = 1;
 
       }else if($gaji>2 && $gaji<=4){
-        $gajiSedikit = round((4-$gaji)/(4-2),2);
+        $gajiSedikit = (4-$gaji)/(4-2);
 
       }else{
         $gajiSedikit = 0;
@@ -72,32 +72,32 @@
       $z6= 0;
     
       $R1=min($masakerjaBaru,$gajiSedikit); //maka bonus sedikit
-      if ($R1>0) $z1 = 600000 - (300000*$R1);
+       $z1 = 600000 - (300000*$R1);
 
       $R2=min($masakerjaBaru,$gajiBanyak); //maka bonus sedikit
-      if ($R2>0) $z2 = 600000 - (300000*$R2);
+       $z2 = 600000 - (300000*$R2);
 
       $R3=min($masakerjaSedang,$gajiSedikit); //maka bonus sedikit
-      if ($R3>0) $z3 = 600000 - (300000*$R3);
+       $z3 = 600000 - (300000*$R3);
 
       $R4=min($masakerjaSedang,$gajiBanyak); //maka bonus banyak
-      if ($R4>0) $z4 = 300000 + $R4*300000;
+       $z4 = 300000 + $R4*300000;
 
       $R5=min($masakerjaLama,$gajiSedikit); //maka bonus banyak
-      if ($R5>0) $z5 = 300000 + $R5*300000;
+      $z5 = 300000 + $R5*300000;
 
       $R6=min($masakerjaLama,$gajiBanyak);
-      if ($R6>0) $z6 = 300000 + $R6*300000;
+      $z6 = 300000 + $R6*300000;
       $total_RiZi = ($R1*$z1)+($R2*$z2)+($R3*$z3)+($R4*$z4)+($R5*$z5)+($R6*$z6);
       $total_R = $R1+$R2+$R3+$R4+$R5+$R6;
-      $z = round($total_RiZi/$total_R,2);
+      $z = $total_RiZi/$total_R;
       
       $bonus = $z;
       if($bonus<=300000){
          $bonusBanyak = 0;
       }
       else if($bonus>300000 && $bonus<=600000){
-        $bonusBanyak=round(($bonus - 300000) /(600000 - 300000),2);
+        $bonusBanyak=($bonus - 300000) /(600000 - 300000);
       }else{
         $bonusBanyak = 1;
       }
@@ -106,7 +106,7 @@
          $bonusSedikit = 1;
       }
       else if($bonus>300000 && $bonus<=600000){
-        $bonusSedikit=round((600000-$bonus) /(600000 - 300000),2);
+        $bonusSedikit=(600000-$bonus) /(600000 - 300000);
       }else{
         $bonusSedikit = 0;
       }
@@ -160,62 +160,62 @@ if($z!=""){
           <tr>
             <td>Rule 1</td>
             <td>Jika Masa Kerja <b>Baru</b> dan Gaji <b>Sedikit</b> Maka Bonus <b>Sedikit</b></td>
-            <td><center><?php echo $masakerjaBaru;?><center></td>
-            <td><center><?php echo $gajiSedikit;?><center></td>
-            <td><center><?php echo $R1;?><center></td>
-            <td><center><?php echo $z1;?><center></td>
-            <td><center><?php echo $R1*$z1;?><center></td>
+            <td><center><?php echo round($masakerjaBaru,2);?><center></td>
+            <td><center><?php echo round($gajiSedikit,2);?><center></td>
+            <td><center><?php echo round($R1,2);?><center></td>
+            <td><center><?php echo round($z1,2);?><center></td>
+            <td><center><?php echo round($R1*$z1,2);?><center></td>
           </tr>
 
           <tr>
             <td>Rule 2</td>
             <td>Jika Masa Kerja <b>Baru</b> dan Gaji <b>Banyak</b> Maka Bonus <b>Sedikit</b></td>
-            <td><center><?php echo $masakerjaBaru;?><center></td>
-            <td><center><?php echo $gajiBanyak;?><center></td>
-            <td><center><?php echo $R2;?><center></td>
-            <td><center><?php echo $z2;?><center></td>
-            <td><center><?php echo $R2*$z2;?><center></td>
+            <td><center><?php echo round($masakerjaBaru,2);?><center></td>
+            <td><center><?php echo round($gajiBanyak,2);?><center></td>
+            <td><center><?php echo round($R2,2);?><center></td>
+            <td><center><?php echo round($z2,2);?><center></td>
+            <td><center><?php echo round($R2*$z2,2);?><center></td>
           </tr>
 
           <tr>
             <td>Rule 3</td>
             <td>Jika Masa Kerja <b>Sedang</b> dan Gaji <b>Sedikit</b> Maka Bonus <b>Sedikit</b></td>
-            <td><center><?php echo $masakerjaSedang;?><center></td>
-            <td><center><?php echo $gajiSedikit;?><center></td>
-            <td><center><?php echo $R3;?><center></td>
-            <td><center><?php echo $z3;?><center></td>
-            <td><center><?php echo $R3*$z3;?><center></td>
+            <td><center><?php echo round($masakerjaSedang,2);?><center></td>
+            <td><center><?php echo round($gajiSedikit,2);?><center></td>
+            <td><center><?php echo round($R3,2);?><center></td>
+            <td><center><?php echo round($z3,2);?><center></td>
+            <td><center><?php echo round($R3*$z3,2);?><center></td>
           </tr>
 
           <tr>
             <td>Rule 4</td>
             <td>Jika Masa Kerja <b>Sedang</b> dan Gaji <b>Banyak</b> Maka Bonus <b>Banyak</b></td>
-            <td><center><?php echo $masakerjaSedang;?><center></td>
-            <td><center><?php echo $gajiBanyak;?><center></td>
-            <td><center><?php echo $R4;?><center></td>
-            <td><center><?php echo $z4;?><center></td>
-            <td><center><?php echo $R4*$z4;?><center></td>
+            <td><center><?php echo round($masakerjaSedang,2);?><center></td>
+            <td><center><?php echo round($gajiBanyak,2);?><center></td>
+           <td><center><?php echo round($R4,2);?><center></td>
+            <td><center><?php echo round($z4,2);?><center></td>
+            <td><center><?php echo round($R4*$z4,2);?><center></td>
           </tr>
 
           <tr>
             <td>Rule 5</td>
             <td>Jika Masa Kerja <b>Lama</b> dan Gaji <b>Sedikit</b> Maka Bonus <b>Banyak</b></td>
-            <td><center><?php echo $masakerjaLama;?><center></td>
-            <td><center><?php echo $gajiSedikit;?><center></td>
-            <td><center><?php echo $R5;?><center></td>
-            <td><center><?php echo $z5;?><center></td>
-            <td><center><?php echo $R5*$z5;?><center></td>
+            <td><center><?php echo round($masakerjaLama,2);?><center></td>
+            <td><center><?php echo round($gajiSedikit,2);?><center></td>
+            <td><center><?php echo round($R5,2);?><center></td>
+            <td><center><?php echo round($z5,2);?><center></td>
+            <td><center><?php echo round($R5*$z5,2);?><center></td>
           </tr>
 
 
           <tr>
             <td>Rule 6</td>
             <td>Jika Masa Kerja <b>Lama</b> dan Gaji <b>Banyak</b> Maka Bonus <b>Banyak</b></td>
-            <td><center><?php echo $masakerjaLama;?><center></td>
-            <td><center><?php echo $gajiBanyak;?><center></td>
-            <td><center><?php echo $R6;?><center></td>
-            <td><center><?php echo $z6;?><center></td>
-            <td><center><?php echo $R6*$z6;?><center></td>
+            <td><center><?php echo round($masakerjaLama,2);?><center></td>
+            <td><center><?php echo round($gajiBanyak,2);?><center></td>
+            <td><center><?php echo round($R6,2);?><center></td>
+            <td><center><?php echo round($z6,2);?><center></td>
+            <td><center><?php echo round($R6*$z6,2);?><center></td>
           </tr>
 
 
@@ -224,13 +224,13 @@ if($z!=""){
             <td>Jumlah</td>
             <td><center><center></td>
             <td><center><center></td>
-            <td><center><?php echo $total_R;?><center></td>
+            <td><center><?php echo round($total_R,3);?><center></td>
             <td><center><center></td>
-            <td><center><?php echo $total_RiZi;?><center></td>
+            <td><center><?php echo round($total_RiZi,2);?><center></td>
           </tr>
           <tr>
             <td></td>
-            <td colspan="6">Nilai Bonus = SUM(Ri*Zi) / SUM(Ri) = <?php echo $total_RiZi." / ".$total_R;?> = Rp. <?php echo number_format($bonus,2,",",".");?> </td>
+            <td colspan="6">Nilai Bonus = SUM(Ri*Zi) / SUM(Ri) = <?php echo round($total_RiZi,2)." / ".round($total_R,3);?> = Rp. <?php echo number_format($bonus,2,",",".");?> </td>
           </tr>
       </table>
     </div>
@@ -241,6 +241,11 @@ if($z!=""){
 
 <?php 
 function displayMasaKerja($masakerjaBaru, $masakerjaSedang, $masakerjaLama){
+
+  $masakerjaBaru = round($masakerjaBaru,2);
+  $masakerjaSedang = round($masakerjaSedang,2);
+  $masakerjaLama = round($masakerjaLama,2);
+
   if ($masakerjaBaru==0){
       echo "<span class='col-md-4 nol'><small>Baru</small> (0)</span>";
   }else{
@@ -265,6 +270,10 @@ function displayMasaKerja($masakerjaBaru, $masakerjaSedang, $masakerjaLama){
 
 <?php 
 function displayGaji($gajiSedikit, $gajiBanyak){
+
+  $gajiSedikit = round($gajiSedikit,2);
+  $gajiBanyak = round($gajiBanyak,2);
+
   if ($gajiSedikit==0){
       echo "<span class='col-md-4 col-md-offset-2 nol'><small>Sedikit</small> (0)</span>";
   }else{
@@ -281,6 +290,9 @@ function displayGaji($gajiSedikit, $gajiBanyak){
 
 <?php 
 function displayBonus($bonusSedikit, $bonusBanyak){
+
+  $bonusSedikit = round($bonusSedikit,2);
+  $bonusBanyak = round($bonusBanyak,2);
   
   if ( $bonusSedikit==0){
       echo "<span class='col-md-4 col-md-offset-2 nol'><small>Sedikit</small> (0)</span>";
